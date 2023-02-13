ab971d7c280b6ea16784fb3eaf4646434ea902b7
patrick-potocny <patrik.potocny85@gmail.com>
2023-02-13 13:35:56 +0100

    Landing Page animations

--- components/AboutApp.tsx
+++ components/AboutApp.tsx
@@ -1,19 +1,50 @@
 import React from "react";
 import Image from "next/image";
-import screenshots from '@/public/images/screenshots.png'
-import styles from '@/styles/components/AboutApp.module.scss'
+import screenshots from "@/public/images/screenshots.png";
+import styles from "@/styles/components/AboutApp.module.scss";
+import { motion } from "framer-motion";
 
 export default function AboutApp() {
   return (
     <div className={styles.aboutAppDiv}>
-      <h1>Make job hunting easier</h1>
-      <p>
-        Jobly helps job seekers stay organized by tracking their job search.
-        From reviewing postings to scheduling interviews, Jobly helps users to 
-        organize all necessary information.
-      </p>
-      <button>Get Started</button>
-      <Image src={screenshots} alt="Screenshots from the app"/>
+      <div className={styles.wrapper}>
+        <motion.div
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          transition={{ duration: .6, delay: 0.6, type: "spring" }}
+        >
+          <h1>Make job hunting easier</h1>
+        </motion.div>
+      </div>
+      <div className={styles.wrapper}>
+        <motion.div
+          initial={{ y: "140%" }}
+          animate={{ y: 0 }}
+          transition={{ duration: .6, delay: 1.1, type: "spring" }}
+        >
+          <p>
+            Jobly helps job seekers stay organized by tracking their job search.
+            From reviewing postings to scheduling interviews, Jobly helps users
+            to organize all necessary information.
+          </p>
+        </motion.div>
+      </div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: .6, delay: 1.9, type: "spring" }}
+        >
+          <button>Get Started</button>
+        </motion.div>
+      <div className={styles.wrapper}>
+        <motion.div
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          transition={{ duration: .6, delay: 1.5, type: "spring" }}
+        >
+          <Image src={screenshots} alt="Screenshots from the app" />
+        </motion.div>
+      </div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,38 @@ import Image from "next/image";
 import logo from "@/public/images/logo.png";
 import styles from "@/styles/LandingPage.module.scss";
 import SignInCard from "@/components/SignInCard";
-import AboutApp from '@/components/AboutApp';
+import AboutApp from "@/components/AboutApp";
+import { motion } from "framer-motion";
 
 export default function LandingPage() {
   return (
+    <>
     <div className={styles.container}>
       <Image className={styles.logo} src={logo} alt="Jobly Logo" />
       <main className={styles.main}>
-        <div className={styles.signInDiv}>
+        <motion.div
+          className={styles.signInDiv}
+          initial={{ x: -500, scale: 0.2, opacity: 0 }}
+          animate={{ opacity: 1, x: 0, scale: 1 }}
+          transition={{
+            delay: 2,
+            duration: 1.5,
+            type: "spring",
+            stiffness: 50,
+          }}
+        >
           <SignInCard />
+        </motion.div>
+        <div className={styles.aboutAppDiv}>
+          <AboutApp />
         </div>
-        <div className={styles.aboutAppDiv}><AboutApp /></div>
       </main>
     </div>
+    <motion.div 
+      className={styles.overlay}
+      animate={{ x: "-100vw" }}
+      transition={{ duration: 1 }}
+    />
+    </>
   );
 }
